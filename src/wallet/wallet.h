302a941639ff668022cc70038b7a3e393bce37c4
CryptoForge <cryptoforge.cc@gmail.com>
2019-05-24 11:09:40 -0700

    Update fees in wallet.h

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -46,11 +46,11 @@ static const unsigned int DEFAULT_KEYPOOL_SIZE = 100;
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;
 //! -fallbackfee default
-static const CAmount DEFAULT_FALLBACK_FEE = 0 ;
+static const CAmount DEFAULT_FALLBACK_FEE = 20000000;
 //! -mintxfee default
-static const CAmount DEFAULT_TRANSACTION_MINFEE = 0 ;
+static const CAmount DEFAULT_TRANSACTION_MINFEE = 10000000;
 //! minimum recommended increment for BIP 125 replacement txs
-static const CAmount WALLET_INCREMENTAL_RELAY_FEE = 5000;
+static const CAmount WALLET_INCREMENTAL_RELAY_FEE = 50000000;
 //! target minimum change amount
 static const CAmount MIN_CHANGE = CENT;
 //! final minimum change amount after paying for fees
